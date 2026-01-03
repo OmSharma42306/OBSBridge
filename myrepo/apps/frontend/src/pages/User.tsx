0745d4805fda232f94ba3bcbc8cc6c0c2d57cd50
Om Sharma <omsharma.83173@gmail.com>
2026-01-03 18:19:22 +0530

    feat:added-host-and-user-pages-and-routes

---
+++ myrepo/apps/frontend/src/pages/User.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function User(){
+    const [roomId,setRoomId] = useState<string>('');
+    const [name,setName] = useState<string>('');
+
+    function handleJoinRoom(){
+        alert(`Room Joined! for Room Number ${roomId} with Name : ${name}`);
+    }
+    return <div> 
+        <h1>Enter RoomId</h1>
+        <br />
+        <input type="text" onChange={(e)=>{setRoomId(e.target.value)}} />
+        <br />
+        <h1>Enter Name</h1>
+        <br />
+        <input type="text" onChange={(e)=>{setName(e.target.value)}} />
+        <br />
+        <button onClick={handleJoinRoom}>Join Room</button>
+    </div>
+}
