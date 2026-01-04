cd9509ac0b5df97ccb243f758fde578a7fad7131
Om Sharma <omsharma.83173@gmail.com>
2026-01-04 21:06:46 +0530

    feat(frontend-pages): add HostDashboard and UserDashboard pages with routing and WS-enabled stream components

--- myrepo/apps/frontend/src/App.tsx
+++ myrepo/apps/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import {Routes,Route} from "react-router-dom"
 import User from "./pages/User"
 import Host from "./pages/Host"
+import HostDashboard from "./pages/HostDashboard"
+import UserDashboard from "./pages/UserDashboard"
 function App() {
   
   return (
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/user" element=<User/>/>
       <Route path="/host" element=<Host/>/>
+      <Route path="/userDashboard" element=<UserDashboard/>/>
+      <Route path="/hostDashboard" element=<HostDashboard/>/>
     </Routes>
       
     </>
