0e561a9a97c870f3e1df85e2dbd81dbf8cf13d69
Om Sharma <omsharma.83173@gmail.com>
2026-02-04 17:39:46 +0530

    feat(mediasoup): add websocket server, init mediasoup starter, and types

--- myrepo/apps/sfu/src/index.ts
+++ myrepo/apps/sfu/src/index.ts
@@ -1,1 +1,38 @@
-// sfu server 
+import * as mediasoup from "mediasoup";
+import { WebSocket,WebSocketServer} from "ws";
+import type {Room,Peer} from "./types.js"
+import type { Router, Worker } from "mediasoup/types";
+// Websocket Server Initialization..
+const wss = new WebSocketServer({port : 8080});
+
+// mediasoup global variables.
+let router : Router | any;
+
+// peers and rooms tracking.
+const peers = new Map<string,Peer>();
+
+const room : Room | any  = {};
+
+// mediasoup worker,router creation.
+(async()=>{
+    const worker : Worker = await mediasoup.createWorker();
+    router = await worker.createRouter({mediaCodecs:[
+        {kind : 'video',mimeType:'video/VP8',clockRate:90000}
+    ]});
+    console.log("Router Created!",router);
+    room[router] = peers;
+})();
+
+
+wss.on('connection',function connection(ws : WebSocket){
+    ws.on('error',console.error);
+
+    ws.on('message',function message(data:any,isBinary){
+        const msg = JSON.parse(data);
+        if(msg.type === "client-join"){
+            ws.send(JSON.stringify({ msg : router,peers}));
+        }
+    })
+
+    ws.send(JSON.stringify({ msg : "success",room}));
+})
